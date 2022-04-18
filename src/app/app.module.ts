77b00e05725435881be6d7e4a9616593147156c5
Marcos <markensi9@outlook.com>
2022-04-18 09:43:11 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArticleComponent } from './components/article/article.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ArticleComponent,
+    UserProfileComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
