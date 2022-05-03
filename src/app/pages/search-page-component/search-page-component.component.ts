793e6b95ede4e191d469f83fc069df2ec94bf047
Marcos <markensi9@outlook.com>
2022-05-03 13:55:29 +0100

    Added routing Added all component to searchPage works Added CSS Added other components Fixed carousel home overlaytext

---
+++ src/app/pages/search-page-component/search-page-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page-component',
+  templateUrl: './search-page-component.component.html',
+  styleUrls: ['./search-page-component.component.css']
+})
+export class SearchPageComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
